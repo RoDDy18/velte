bead24af8329f90be82ba81e7cb30516c889c0af
RoDDy18 <damilolaoni2004@gmail.com>
2024-01-25 18:49:24 +0100

    v2.0.0-rc.1 (release candidate version patch)

--- types/velte/velx/store.d.ts
+++ types/velte/velx/store.d.ts
@@ -1,5 +1,5 @@
 /*
- *  velte v2.0.0-rc.0
+ *  velte v2.0.0-rc.1
  *  A Lightweight, Event-driven, Performant, Frontend framework for Modern Apps.
  *  Copyright (c) 2023 Emmanuel Oni
  *  Licence - https://github.com/RoDDy18/velte/blob/main/LICENSE
